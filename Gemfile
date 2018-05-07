f43b7f0ec5d8567c3a0b57ac550ffa3d69c5b840
hoshi-sano <hoshi.sanou@gmail.com>
2018-05-07 19:09:10 +0900

    開発用にデフォルトBGM/SEを外部から取得できるようにした

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'dxruby', '~> 1.4.0'
 gem 'rake', '>= 12.3.1'
+gem 'rubyzip', '>= 1.0.0'
+gem 'certified', '>= 1.0.0'
--- lib/tasks/development.rb
+++ lib/tasks/development.rb
@@ -1,3 +1,17 @@
+MD_BASE_URL = 'https://maoudamashii.jokersounds.com/music'
+BGM_LIST = {
+  title:   '/bgm/mp3/bgm_maoudamashii_8bit29.mp3',
+  dungeon: '/bgm/mp3/bgm_maoudamashii_8bit28.mp3',
+}
+SE_LIST = {
+  break:    '/se/wav/se_maoudamashii_retro22.wav', # ブロック破壊音
+  got_item: '/se/wav/se_maoudamashii_retro08.wav', # アイテム取得音
+  jump:     '/se/wav/se_maoudamashii_retro03.wav', # ジャンプ
+  ok:       '/se/wav/se_maoudamashii_retro16.wav', # 決定音
+  pre_bomb: '/se/wav/se_maoudamashii_retro24.wav', # 爆発前兆音
+}
+
+
 # dxruby.soファイルが配置されているディレクトリを返す
 def dxruby_so_dir
   $LOAD_PATH.each do |path|
@@ -5,6 +19,22 @@ def dxruby_so_dir
     return path if File.exists?(dxruby_so_path)
   end
   raise 'cannot find dxruby.so'
+end
+
+# 魔王魂さまよりBGM/SE用ファイルを取得し指定したディレクトリに配置する
+def fetch_md_file(md_file_name, dist)
+  return if File.exists?(dist)
+  url = File.join(MD_BASE_URL, md_file_name)
+  fetch_binary_file(url, dist)
+end
+
+def fetch_binary_file(url, dist)
+  open(url) do |file|
+    open(dist, 'wb') do |out|
+      out.write(file.read)
+      puts "got #{url}, and wrote #{dist}"
+    end
+  end
 end
 
 namespace :dev do
@@ -16,6 +46,21 @@ namespace :dev do
     else
       puts "copy #{args[:ayame_so_path]} to #{dir}"
       FileUtils.copy(args[:ayame_so_path], dir)
+    end
+  end
+
+  desc '魔王魂さまよりBGMやSEなどの必要なファイルを取得する'
+  task :fetch_md do
+    require 'zip'
+    require 'open-uri'
+    require 'certified'
+    [[BGM_LIST, File.join(ROOT, 'data', 'musics')],
+     [SE_LIST,  File.join(ROOT, 'data', 'sounds')]].each do |list, dist_dir|
+      list.each do |dist_file_name, md_file_name|
+        ext = File.extname(md_file_name)
+        dist = File.join(dist_dir, "#{dist_file_name}#{ext}")
+        fetch_md_file(md_file_name, dist)
+      end
     end
   end
 end
