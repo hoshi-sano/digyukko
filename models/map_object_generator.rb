b05f247b90ac1af40b3222585e8d453a93495124
hoshi-sano <hoshi.sanou@gmail.com>
2019-06-11 01:14:02 +0900

    マップオブジェクトの排出率管理用のクラスを追加

--- models/map_object_generator.rb
+++ models/map_object_generator.rb
@@ -1,36 +1,82 @@
 module DigYukko
   class MapObjectGenerator
+    # ランダムマップ生成時の破壊可能オブジェクト排出率管理用クラス
+    class BreakableObjectTable < Hash
+      DEFAULT_SCORE = [
+        [ProjectileCostumeItem, 2],
+        [ItemBox,               2],
+        [LifeRecoverItem,       2],
+        [WideSpreadBomb,        8],
+        [BreakableBlock,        80],
+      ]
+
+      def initialize
+        super
+        init_score
+      end
+
+      def []=(key, val)
+        @max_score = nil
+        super
+        reset_stock
+      end
+
+      # オブジェクトの排出スコアを初期化する
+      def init_score
+        self.class::DEFAULT_SCORE.each { |obj, score| self[obj] = score }
+      end
+
+      def draw(val)
+        @stock.find { |_, score| val <= score }[0]
+      end
+
+      def max_score
+        @max_score ||= self.values.inject(&:+)
+      end
+
+      # 排出スコアを積算して通し番号にする
+      def reset_stock
+        @stock = {}
+        obj_to_score_ary = self.dup.to_a
+        prev = nil
+        @stock = obj_to_score_ary.map { |obj, score|
+          new_score = score + (prev || 0)
+          prev = new_score
+          [obj, new_score]
+        }.to_h
+      end
+    end
+
+    # アイテム排出率管理用クラス
+    class ItemTable < BreakableObjectTable
+      DEFAULT_SCORE = [
+        [ProjectileCostumeItem, 2],
+        [LifeRecoverItem,       2],
+      ]
+    end
+
     def initialize(yukko)
       @yukko = yukko
+      @breakable_object_table = BreakableObjectTable.new
+      @item_table = ItemTable.new
     end
 
     # ItemBoxなどから利用するメソッド
     # TODO: yukkoの状態やスコア等を考慮しつつ、ややランダムにアイテムのクラスを返す
     def generate_item
-      LifeRecoverItem
+      @item_table.draw(random(@item_table.max_score))
     end
 
     # 破壊可能なブロック、爆弾、アイテムのいずれかのクラスを返す
-    # TODO: いい感じの確率で返すようにする
+    # TODO: マップ進行度やスコア等を考慮してマップ構成を変更する
     def breakable_object_class
-      val = random(100)
-      if val > 98
-        ProjectileCostumeItem
-      elsif val > 95
-        ItemBox
-      elsif val > 93
-        LifeRecoverItem
-      elsif val > 85
-        WideSpreadBomb
-      else
-        BreakableBlock
-      end
+      @breakable_object_table.draw(random(@breakable_object_table.max_score))
     end
 
     private
 
     def random(max)
-      ApplicationManager.random_number_generator.rand(0..max)
+      ApplicationManager.random_number_generator.rand(1..max)
     end
   end
 end
