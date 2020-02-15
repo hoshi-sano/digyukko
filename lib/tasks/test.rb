41760dd8b63f3d34ef8d45e77131123fa9c61e41
hoshi-sano <hoshi.sanou@gmail.com>
2020-02-16 01:55:07 +0900

    rake testの不備を修正

--- lib/tasks/test.rb
+++ lib/tasks/test.rb
@@ -3,7 +3,7 @@ require 'rake/testtask'
 desc 'Run test-unit based test'
 Rake::TestTask.new(:test) do |t|
   t.libs << "test"
-  t.test_files = FileList['test/**/test*.rb']
+  t.test_files = FileList['test/**/*_test.rb']
   t.verbose = true
   t.warning = true
 end
