08cc4bb271aee706e4ac670b1c10cf2b5cd6f8c0
hoshi-sano <hoshi.sanou@gmail.com>
2020-02-26 01:07:00 +0900

    ProjectileCostume用のアイテム画像を追加

--- models/projectile_costume_item.rb
+++ models/projectile_costume_item.rb
@@ -1,9 +1,6 @@
 module DigYukko
   class ProjectileCostumeItem < Item
-    # TODO: 専用の画像を用意する
-    set_image ::DXRuby::Image.new(32, 32).tap { |img|
-      img.circle_fill(16, 16, 16, ::DXRuby::C_YELLOW)
-    }
+    set_image load_image('projectile_item')
     set_score 10
     set_power 0
     fragment(image)
