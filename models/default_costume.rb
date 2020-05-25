101059d0611d5a20ca246e9e08aa10d8b68599d3
hoshi-sano <hoshi.sanou@gmail.com>
2020-05-25 12:16:54 +0900

    DefaultCostume特殊技の位置調整

--- models/default_costume.rb
+++ models/default_costume.rb
@@ -16,6 +16,29 @@ module DigYukko
     class ExtraWeapon < ::DigYukko::Costume::ExtraWeapon
       X_IMAGE = load_image('default_extra_weapon')
       Y_IMAGE = X_IMAGE
+
+      def enable(key_x, key_y)
+        if key_y.zero?
+          if key_x > 0 || @yukko.x_dir > 0
+            self.x = @yukko.x + @yukko.width
+          elsif key_x < 0 || @yukko.x_dir < 0
+            self.x = @yukko.x - self.image.width
+          end
+          self.image = self.class::X_IMAGE
+          self.y = @yukko.y
+        else
+          if key_x > 0 || @yukko.x_dir > 0
+            self.x = @yukko.x
+          elsif key_x < 0 || @yukko.x_dir < 0
+            self.x = @yukko.x - (self.image.width - @yukko.width)
+          end
+          self.image = self.class::Y_IMAGE
+          self.y = (key_y < 0) ? (@yukko.y - self.image.height) : @yukko.foot_y
+        end
+        self.visible = true
+        self.collision_enable = true
+        true
+      end
     end
 
     def item_table
